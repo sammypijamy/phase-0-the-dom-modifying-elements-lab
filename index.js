2e3ca478e341015dc5201ab92dc92ce86cce28b0
sammypijamy <samaragransky@outlook.com>
2022-09-20 01:50:57 -0700

    commit tree 2efc244f8bd11525d299e18f5e60e59fe2cfd45a

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+main.remove()
+
+const newHeader = document.createElement('h1');
+document.body.append(newHeader);
+
+newHeader.setAttribute("id","victory");
+
+document.getElementById("victory").textContent = 'Samara Gransky is the champion';
+
+
+
